f04b9d83b5a9bd476aa1ddebbfa57343a56113ce
Scriper.irk <scriper.irk@gmail.com>
2022-02-18 23:09:58 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket =1150;
+        int miles = 20;
+
+        int bonus = ticket / miles;
+        System.out.println("Сумма бесплатных миль за покупку билета равна: " + bonus);
+
+    }
 }
