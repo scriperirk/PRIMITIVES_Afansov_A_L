654f1e0ec1c5972be8bb7143d152e42412afb5f4
Anton <97538274+scriperirk@users.noreply.github.com>
2022-02-21 20:37:41 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket =1150;
-        int miles = 20;
+        int minmiles = 20;
 
-        int bonus = ticket / miles;
-        System.out.println("Сумма бесплатных миль за покупку билета равна: " + bonus);
+        int miles = ticket / minmiles;
+        System.out.println("Сумма бесплатных миль за покупку билета равна: " + miles);
 
     }
 }
